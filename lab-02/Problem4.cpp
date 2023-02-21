bf63854182b32fdb11482a5b8c124316387d4464
eldiiar_b <eldiiar_b@auca.kg>
2023-02-21 15:10:50 +0600

    lab-02, Problem4.cpp Accepted

--- lab-02/Problem4.cpp
+++ lab-02/Problem4.cpp
@@ -3,39 +3,37 @@
 
 using namespace std;
 
-static int map[1000000];
+
 int main()
 {
     int T, count = 1;
-    while(cin >> T) {
+    while(cin >> T && T != 0) {
+        int t, n;
+        vector<int> map(1000000);
         for (int i = 1; i <= T; i++) {
-            int n;
             cin >> n;
             for (int j = 0; j < n; j++) {
-                int t;
                 cin >> t;
                 map[t] = i;
             }
         }
         queue<int> teamControl;
-        queue<int> teams[1100];
+        queue<int> teams[1000];
         cout << "Scenario #" << count++ << "\n";
+        string str;
+        int number;
         while (true) {
-            string str;
             cin >> str;
             if (str[0] == 'S')
                 break;
             else if (str[0] == 'E') {
-                int number;
                 cin >> number;
                 if (teams[map[number]].empty()) {
                     teamControl.push(map[number]);
                 }
                 teams[map[number]].push(number);
-            }else
-            if (str[0] == 'D') {
-                int result = teams[teamControl.front()].front();
-                cout << result << "\n";
+            }else if (str[0] == 'D') {
+                cout << teams[teamControl.front()].front() << "\n";
                 teams[teamControl.front()].pop();
                 if (teams[teamControl.front()].empty())
                     teamControl.pop();
